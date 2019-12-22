9fd02fd56c508a164f13cdcaacfc8dbc84f0e17e
Omega Rae <omegadoesmusic@gmail.com>
2019-12-21 18:35:32 -0800

    should work again but doesnt

--- index.js
+++ index.js
@@ -3,28 +3,39 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component {
-  constructor(props) {
-  	super(props);
-  	this.state = {
-  		value: null,
-  	};
-  }
-
   render() {
     return (
 	  <button 
 	  	className="square" 
-	  	onClick={() => this.setState({value: 'X'})}
+	  	onClick={() => this.props.onClick()}
 	  >
-        {this.state.value}
+        {this.props.value}
       </button>
 	);
   }
 }
 
 class Board extends React.Component {
+  constructor(props) {
+  	super(props);
+  	this.state = {
+  		square: Array(9).fill(null),
+  	};
+  }
+
+  handleClick(i) {
+  	const squares = this.state.squares.slice();
+  	squares[i] = 'X';
+  	this.setState({squares: squares});
+  }
+
   renderSquare(i) {
-    return <Square value={i} />;
+    return (
+    	<Square 
+    		value={this.state.squares[i]} 
+    		onClick={() => this.handleClick(i)}
+    	/>
+    );
   }
 
   render() {
