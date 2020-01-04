141ea9a2b2a31b16930fc527077d6bfda00ec6d9
Omega Rae <omegadoesmusic@gmail.com>
2020-01-03 18:34:58 -0800

    ttt working, move list started

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function Square(props) {
 
 class Board extends React.Component {  
   renderSquare(i) {
-    return (
+    return ( //after update Square gives an error game.Return <board> didn't get the values needed to render new syntax
       <Square 
         value={this.props.squares[i]}
         onClick={() => this.props.onClick(i)}
@@ -49,9 +49,9 @@ class Game extends React.Component {
     super(props);
     this.state = {
       history: [{
-        squares: Array(9).fill(null),
+        squares: Array(9).fill(null)
       }],
-      xIsNext: true,
+      xIsNext: true
     };
   }
 
@@ -76,6 +76,17 @@ class Game extends React.Component {
     const current = history[history.length - 1];
     const winner = calculateWinner(current.squares);
 
+    const moves = history.map((step, move) => {
+      const desc = move ?
+        'Go to move #' + move :
+        'Go to game start';
+      return (
+        <li>
+          <button onClick={() => this.jumpTo(move)}>{desc}</button>
+        </li>
+      );
+    });
+
     let status;
     if (winner) {
       status= 'Winner: ' + winner;
@@ -86,11 +97,15 @@ class Game extends React.Component {
     return (
       <div className="game">
         <div className="game-board">
-          <Board />
+          <Board 
+            squares={current.squares}
+            onClick={(i) => this.handleClick(i)}
+          />
+
         </div>
         <div className="game-info">
           <div>{status}</div>
-          <ol>{/* TODO */}</ol>
+          <ol>{moves}</ol>
         </div>
       </div>
     );
