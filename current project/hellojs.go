a6c95399d56397bb01af9be18c2c518444873ffd
Omega Rae <omegadoesmusic@gmail.com>
2020-01-12 21:48:38 -0800

    new Go proj to have JS serve a pages. go says hello javascript to console

---
+++ current project/hellojs.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmp"
+
+// prints "hello, javescript to the console"
+func main() {
+	fmt.Println("hello, javascript")
+}
+
+/* next steps making a reactjs file that can produce this on a web page. */
