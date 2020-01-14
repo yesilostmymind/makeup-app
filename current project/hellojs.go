d3d24510f1726dce02fc88e42b84d274cf3e1757
Omega Rae <omegadoesmusic@gmail.com>
2020-01-13 22:41:59 -0800

    new webpage project using go with gorilla mux, not currently working

--- current project/hellojs.go
+++ current project/hellojs.go
@@ -1,10 +1,29 @@
+/*
+not currently compiling, error "inconsistent vendoring in C:\Go\src:
+        go.mod requires github.com/gorilla/mux v1.7.3 but vendor/modules.txt does not include it.
+        run 'go mod tidy; go mod vendor' to sync" further errors upon the suggested run comand
+*/
+
 package main
 
-import "fmp"
+//packages, other Go source files you want to use are added with import paths
 
-// prints "hello, javescript to the console"
-func main() {
-	fmt.Println("hello, javascript")
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+//this is a function called homeLink will display "welcome home!" 
+func homeLink(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome home!")
 }
 
-/* next steps making a reactjs file that can produce this on a web page. */
+//this is the main function, it servers homeLink on port 8080
+func main() {
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/", homeLink)
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
