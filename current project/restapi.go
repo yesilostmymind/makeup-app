8744721dd124d9e1183c29a2e1132a5d5fdfd5d0
Omega Rae <omegadoesmusic@gmail.com>
2020-01-19 00:42:51 -0800

    http://localhost:6767/people now serves the storred array as expexted

--- current project/restapi.go
+++ current project/restapi.go
@@ -7,22 +7,22 @@ import (
 
 	"github.com/gorilla/mux"
 )
-
+/*jfc the reason it wasn't building was that the portion after string needed to be with a "backquote" `` its the one where the tilde key is  */
 //struct are used for grouping data together, a collection of fields like first name, last name, address.. for a person
 type Person struct {
-	ID 		  string   'json:"id,omitempty"'
-	Fristname string   'json:"firstname,omitempty"'
-	Lastname  string   'json:"lastname,omitempty"'
-	Address   *Address 'json:"address,omitempty"'
+	ID 		  string   `json:"id,omitempty"`
+	Firstname string   `json:"firstname,omitempty"`
+	Lastname  string   `json:"lastname,omitempty"`
+	Address   *Address `json:"address,omitempty"`
 }
 
 //here is the address struct
 type Address struct {
-	City  string 'json:"city,omitempty"'
-	State string 'json:"state,omitempty"'
+	City  string `json:"city,omitempty"`
+	State string `json:"state,omitempty"`
 }
 
-//the universal verable people is declated, it is a slice of person
+//the universal verable people is declated, it is a slice of
 var people []Person
 
 func GetPersonEndpoint(w http.ResponseWriter, req *http.Request) {
@@ -33,7 +33,7 @@ func GetPersonEndpoint(w http.ResponseWriter, req *http.Request) {
 			return
 		}
 	}
-	json.NewEncoder(w).Encoder(&Person{})
+	json.NewEncoder(w).Encode(&Person{})
 }
 
 func GetPeopleEndpoint(w http.ResponseWriter, req *http.Request){
@@ -45,7 +45,7 @@ func CreatePersonEndpoint(w http.ResponseWriter, req *http.Request) {
 	params := mux.Vars(req)
 	var person Person
 	_ =json.NewDecoder(req.Body).Decode(&person)
-	perosn.ID = params["id"]
+	person.ID = params["id"]
 	people = append(people, person)
 	json.NewEncoder(w).Encode(people)
 }
@@ -54,7 +54,7 @@ func DeletePersonEndpoint(w http.ResponseWriter, req *http.Request) {
 	params := mux.Vars(req)
 	for index, item := range people {
 		if item.ID == params["id"] {
-			people = append(people[:index], people[index+1]...)
+			people = append(people[:index], people[index+1:]...)
 			break
 		}
 	}
@@ -63,8 +63,8 @@ func DeletePersonEndpoint(w http.ResponseWriter, req *http.Request) {
 
 func main() {
 	router := mux.NewRouter()
-	people = append(people, Person{ID: "1", Fristname: "Omega", Lastname: "Rae", Address: &Address{City: "Daly City", State: "CA"}})
-	people = append(people, Person{ID: "2", Fristname: "Kitty", Lastname: "Cat"})
+	people = append(people, Person{ID: "1", Firstname: "Omega", Lastname: "Rae", Address: &Address{City: "Daly City", State: "CA"}})
+	people = append(people, Person{ID: "2", Firstname: "Kitty", Lastname: "Cat"})
 	router.HandleFunc("/people", GetPeopleEndpoint).Methods("GET")
 	router.HandleFunc("/people/{id}", GetPersonEndpoint).Methods("GET")
 	router.HandleFunc("/people/{id}", CreatePersonEndpoint).Methods("POST")
