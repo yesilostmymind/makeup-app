3bb9b55bf5bf6b34faa3d8e10a7bc7bb1d05ba01
Omega Rae <omegadoesmusic@gmail.com>
2020-01-31 23:55:54 -0800

    removed excess cards still trying to get to talk to go

--- current project/frontend.js
+++ current project/frontend.js
@@ -2,14 +2,16 @@ import React, { Component } from "react";
 import axios from "axios";
 import { Card, Header, Form, Input, Icon } from "semantic-ui-react";
 
+let endpoint = "http://localhost:8080"
+
 const CardItems = () (
 	//<Segment inverted> dark background not working yet
 		<Card.Group itmesPerRow={1}>
 			<Card color='purple'> 
 				<Card.Content>
 					<Image floated='right'/>
-					<Card.Header> </Card.Header> //this will be the item name
-					<Card.Description> </Card.Description> //this will be the item Description
+					<Card.Header>{.Title}</Card.Header> //this will be the item name
+					<Card.Description>{.Description}</Card.Description> //this will be the item Description
 				</Card.Content>
 				<Card.Content extra>
 					<div className='ui three buttons'>
@@ -28,87 +30,12 @@ const CardItems = () (
 	      			</div>
 				</Card.Content>
 			</Card>
-	    	<Card color='purple'>
-		    	<Card.Content>
-						<Card.Header> </Card.Header>
-						<Card.Description> </Card.Description>
-				</Card.Content>
-				<Card.Content extra>
-					<div className='ui three buttons'>
-						<Button inverted color='blue' icon>
-							<Icon name='heart' />
-	        				have it
-	      				</Button>
-	      				<Button inverted color='pink' icon>
-	      					<Icon name='star' />
-	        				want it
-	      				</Button>
-	      				<Button inverted color="violet" icon>
-	      					<Icon name='fork' />
-	      					more info
-	      				</Button>
-      				</div>
-				</Card.Content>
-	    	</Card>
-	    	<Card color='purple'>
-		    	<Card.Content>
-					<Card.Header> </Card.Header>
-					<Card.Description> </Card.Description>
-				</Card.Content>
-				<Card.Content extra>
-					<div className='ui three buttons'>
-					<Button inverted color='blue'>
-		        		Have it
-		      		</Button>
-		      		<Button inverted color='pink'>
-		        		want it
-		      		</Button>
-		      		<Button inverted color="violet">
-		      			more info
-		      		</Button>
-				</Card.Content>
-	    	</Card>
-	    	<Card color='purple'>
-	    		<Card.Content>
-					<Card.Header> </Card.Header>
-					<Card.Description> </Card.Description>
-				</Card.Content>
-				<Card.Content extra>
-					<Button inverted color='blue'>
-        				Have it
-      				</Button>
-      				<Button inverted color='pink'>
-        				want it
-      				</Button>
-      				<Button inverted color="violet">
-      					more info
-      				</Button>
-				</Card.Content>
-	    	</Card>
-	    	<Card color='purple'>
-	    		<Card.Content>
-					<Card.Header> </Card.Header>
-					<Card.Description> </Card.Description>
-				</Card.Content>
-				<Card.Content extra>
-					<Button inverted color='blue'>
-        				Have it
-      				</Button>
-      				<Button inverted color='pink'>
-        				want it
-      				</Button>
-      				<Button inverted color="violet">
-      					more info
-      				</Button>
-				</Card.Content>
-	    	</Card>
+
     	</Card.Group>
     //</Segment>
 )
 
 /*
-let endpoint = "http://localhost:8080";
-
 class MakeupDataBase extends Component {
 	constructor(props) {
 		super(props);
